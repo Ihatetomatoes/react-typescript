b9e11bd86e4103b8d61e989949de7b8d7c1f77ec
Petr Tichy <petr.tichy@carsales.com.au>
2017-02-20 16:09:04 +1100

    production and development env

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,71 @@
-var webpack = require('webpack');
-var ForkCheckerPlugin = require('awesome-typescript-loader');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ForkCheckerPlugin = require('awesome-typescript-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-var path = require('path');
+const path = require('path');
 
 var config = {
     app: ['./src/index.tsx'],
     tsxLoaders: ['babel-loader','awesome-typescript-loader']
 }
 
-var dev = process.env.NODE_ENV !== 'production';
+module.exports = (env) => {
 
-module.exports = {
-    entry: {
-        app: config.app
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle-[hash].js',
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.webpack.js', '.scss']
-    },
-    devServer: {
-        port: 8080,
-        stats: 'errors-only'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-        {
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: config.tsxLoaders
+    const isDevelopment = env.development === true;
+    const isProduction = env.production === true;
+
+    return {
+        entry: {
+            app: config.app
         },
-        {
-            test: /mobx-react-devtools/,
-            use: dev ? 'noop' : 'null'
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle-[hash].js',
+            publicPath: '/'
         },
-        {
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: ['css-loader?modules&camelCase', 'sass-loader']
-            })
-        }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'React with TypeScript Starter Kit',
-            template: './src/index.ejs'
-        }),
-        new (webpack.optimize.OccurenceOrderPlugin || webpack.optimize.OccurrenceOrderPlugin)(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.webpack.js', '.scss']
+        },
+        devServer: {
+            port: 8080,
+            stats: 'errors-only'
+        },
+        devtool: (() => {
+            if (isProduction) return 'hidden-source-map'
+            else return 'cheap-module-eval-source-map'
+        })(),
+        module: {
+            loaders: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: config.tsxLoaders
+            },
+            {
+                test: /mobx-react-devtools/,
+                use: isDevelopment ? 'noop' : 'null'
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader?modules&camelCase', 'sass-loader']
+                })
             }
-        }),
-        new ExtractTextPlugin("styles.css")
-    ]
+            ]
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                // Allows these constants to be accessed by the aurelia app
+                DEVELOPMENT: JSON.stringify(isDevelopment),
+                PRODUCTION: JSON.stringify(isProduction)
+            }),
+            new HtmlWebpackPlugin({
+                title: 'React with TypeScript Starter Kit',
+                template: './src/index.ejs'
+            }),
+            new (webpack.optimize.OccurenceOrderPlugin || webpack.optimize.OccurrenceOrderPlugin)(),
+            new ExtractTextPlugin("styles.css")
+        ]
+    }
 };
